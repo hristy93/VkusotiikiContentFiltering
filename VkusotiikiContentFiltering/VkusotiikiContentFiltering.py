34aefdfe432655f333729f8fe8cc927a5e342dca
Milka Ferezliyska <mileto94@abv.bg>
2017-02-10 13:26:58 +0200

    Create likes file

--- VkusotiikiContentFiltering/VkusotiikiContentFiltering.py
+++ VkusotiikiContentFiltering/VkusotiikiContentFiltering.py
@@ -814,6 +814,14 @@ def prepare_data():
     }
 
 
+def get_recipes_names(data):
+    names = []
+
+    # for recipe in data:
+    #     names.append(recipe.get('name'))
+    return [recipe.get('name') for recipe in data]
+
+
 def main():
     fetched_data = prepare_data()
     data = fetched_data.get('data')
---
+++ VkusotiikiContentFiltering/create_likes_file.py
@@ -0,0 +1,31 @@
+import xlsxwriter
+
+from VkusotiikiContentFiltering import prepare_data, get_recipes_names
+
+
+def create_file():
+    """Create file with recipes ids, recipes titles and default values for likes."""
+    data = prepare_data().get('data')
+    recipes_names = get_recipes_names(data)
+    print(len(recipes_names))
+    print(recipes_names[0])
+
+    # Create an new Excel file and add a worksheet.
+    workbook = xlsxwriter.Workbook('recipes.xlsx')
+    worksheet = workbook.add_worksheet()
+
+    # Add a bold format to use to highlight cells.
+    bold = workbook.add_format({'bold': True})
+
+    # Write some simple text.
+    worksheet.write_row(0, 0, range(len(recipes_names)), bold)
+    worksheet.write_row(1, 0, recipes_names, bold)
+
+    for i in range(10):
+        worksheet.write_row(i + 2, 0, [0] * len(recipes_names))
+
+    workbook.close()
+
+
+if __name__ == '__main__':
+    create_file()
